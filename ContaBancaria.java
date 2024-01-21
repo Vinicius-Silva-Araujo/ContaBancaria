67e071df2694a472081fc0eeb423a77e25d7b326
Vinicius Silva Araujo <141644642+Vinicius-Silva-Araujo@users.noreply.github.com>
2024-01-21 19:55:11 -0300

    Update ContaBancaria.java

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class ContaBancaria {
+import java.util.Scanner;
+public class ContaBancaria {
+           public static void main(String[] args) {
+            Scanner leitura = new Scanner(System.in);
+
+            String nome = "vinicius";
+            String tipoConta = "corrente";
+            double saldo = 2500;
+            int escolha = 0;
+
+            System.out.println("Nome "+ nome);
+            System.out.println("Conta"+ tipoConta);
+            System.out.println("Saldo atual "+saldo);
+
+
+            while (escolha != 4){
+                System.out.println("#### Operações ####");
+                System.out.println("1- Consultar saldos");
+                System.out.println("2- Receber valor");
+                System.out.println("3- Transferir valor");
+                System.out.println("4- Sair");
+                escolha = leitura.nextInt();
+
+                if(escolha == 1){
+                    System.out.println("Seu saldo atual é "+saldo);
+                }
+                else if (escolha == 2){
+                    System.out.println("informa o valor a recebido ");
+                    double recebido = leitura.nextDouble();
+                    double saldoAtual = saldo+recebido;
+                    System.out.println("Seu saldo Ataul é "+ saldoAtual);
+                    saldo = saldoAtual;
+                }
+                else if (escolha == 3) {
+                    System.out.println("informa o valor que deseja transferir ");
+                    double valorTrasferir = leitura.nextDouble();
+                    if(saldo< valorTrasferir){
+                        System.out.println("Saldo insufciente ");
+                    }
+                    else {
+                        System.out.println("transferencia realizada com sucesso");
+                        double saldo2 = saldo-valorTrasferir;
+                        saldo = saldo2;
+                    }
+                }
+                else if (escolha == 4) {
+                    System.out.println("Sair do sistema ");
+
+                }
+            }
+           }
 }
+
+
